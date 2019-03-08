3b16fe6196d46dc904b9bc534f7717e54dc0b1bc
emrig <emrig@users.noreply.github.com>
2019-03-08 01:48:51 -0500

    env updates

--- relate/settings.py
+++ relate/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+TYPES = ['PERSON', 'ORGANIZATION', 'LOCATION']
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -79,11 +81,11 @@ WSGI_APPLICATION = 'relate.wsgi.application'
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'relate4',
-            'USER': 'postgres',
-            'HOST': 'localhost',
-            'PASSWORD': 'pass',
-            'PORT': '5432'
+            'NAME': os.environ['POSTGRES_DB'],
+            'USER': os.environ['POSTGRES_USER'],
+            'HOST': os.environ['POSTGRES_HOST'],
+            'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+            'PORT': os.environ['POSTGRES_PORT']
     }
 }
 
