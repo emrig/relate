ef40f356cbeb695169e95aa90db22249fcf57ac7
emrig <emrig@users.noreply.github.com>
2019-03-12 04:55:12 -0400

    settings

--- relate/settings.py
+++ relate/settings.py
@@ -12,7 +12,14 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+# Types of entities to extract
 TYPES = ['PERSON', 'ORGANIZATION', 'LOCATION']
+
+# Clusters per page
+CLUSTER_PAGE_SIZE = 20
+
+# Number of documents to feed to Stanford NER at a time + store in DB
+NER_BATCH_SIZE = 500
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,8 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'discover.apps.DiscoverConfig',
-    'worker.apps.WorkerConfig'
+    'worker.apps.WorkerConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -125,5 +134,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- relate/urls.py
+++ relate/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('discover.urls'))
-]
+    path('', include('discover.urls')),
+] + staticfiles_urlpatterns()
