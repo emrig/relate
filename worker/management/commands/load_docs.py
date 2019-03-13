97e41c9fcb6fa4e0a841f45251d4f2c7350d01dc
emrig <emrig@users.noreply.github.com>
2019-03-13 00:13:46 -0400

    update worker

--- worker/management/commands/load_docs.py
+++ worker/management/commands/load_docs.py
@@ -1,20 +1,59 @@
 from django.core.management.base import BaseCommand
 from datetime import datetime
-from worker.load_docs_from_dir import get_file_paths
+from worker.load_docs_from_dir import get_file_paths, get_file_text
 from worker.queries import insert_docs
+from relate.settings import DOC_DIR, FILE_READ_BATCH_SIZE, TYPES, CLUSTERING_ALGORITHM
+from discover.models import Entity, Cluster
+from worker import queries
+import os
+from worker.worker import parse
+from worker.resolution import Clustering
 
 class Command(BaseCommand):
     help = 'works'
 
     def add_arguments(self, parser):
-        parser.add_argument('total', type=int, help='Indicates the number of users to be created')
+        parser.add_argument('status', type=int, help='Indicates the number of users to be created')
 
     def handle(self, *args, **kwargs):
-        print('GETTING DOCS', kwargs['total'])
-        directory = 'C:/Github/Masters/enron/maildir'
+        directory = os.path.abspath(DOC_DIR)
+        print(f'Scanning Documents in {directory}', kwargs['status'])
+
+        t1 = datetime.now()
+        docs = get_file_paths(directory)
+
+        while len(docs) > 0:
+            batch = docs[:FILE_READ_BATCH_SIZE]
+            docs = docs[FILE_READ_BATCH_SIZE:]
+
+            batch = get_file_text(batch)
+
+            t2 = datetime.now()
+            insert_docs(batch)
+            print('Batch size', FILE_READ_BATCH_SIZE, 'took:', datetime.now() - t2)
+
+        print(datetime.now() - t1)
+
+        print('Extracting entities from documents..')
+
+        parse()
+
         t1 = datetime.now()
 
-        docs = get_file_paths(directory)
-        insert_docs(docs)
+        for type in TYPES:
+            print('Resolving names of type', type)
+            algorithm = CLUSTERING_ALGORITHM
+
+            # Get document counts
+            entities = Entity.objects.all().filter(type=type, visible=True).prefetch_related('documents')
+            entities = [(entity, (entity.name, len(entity.documents.all()))) for entity in entities]
+
+            # Create cluster object
+            clustering = Clustering(algorithm=algorithm, type=type)
+            clusters = clustering.get_clusters(entities=entities)
+
+            # Remove clusters of same type, then delete
+            Cluster.objects.filter(type=type, algorithm=algorithm).delete()
+            queries.insert_clusters(clusters)
 
         print(datetime.now() - t1)
